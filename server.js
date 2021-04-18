bfcdc4298616002efbe3b8fb11fbeeefbe757249
Janmejay16 <54494250+Janmejay16@users.noreply.github.com>
2021-04-18 12:41:30 +0530

    email verfication added

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const bcrypt = require('bcrypt')
 var crypto = require("crypto");
 var nodemailer = require('nodemailer')
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }))
